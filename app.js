0c9ebb6eb7cc04f26fda64af820aa52c8c39f76a
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-03-22 12:57:08 +0100

    changes in port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.listen(3002, () => {
-    console.log('Server is running on port 3002');
+app.listen(3005, () => {
+    console.log('Server is running on port 3005');
   });
   
